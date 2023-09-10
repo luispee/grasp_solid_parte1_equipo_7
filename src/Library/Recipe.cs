b4eb0ba7e438a4c5a520d9b072bc2e8401cfd584
luispee <lpgaudiano99@gmail.com>
2023-09-10 04:54:12 +0200

    correccion Recipe.TotalCost

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,17 +25,26 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        //Calculo el total de los pasos de la receta por separado de la impresion
+        public double TotalCost { get 
+            {
+                double totalCost = 0;
+                foreach (Step step in this.steps)
+                {
+                    totalCost += step.GetProductionCost(step);
+                }
+                return totalCost;
+            } 
+        }
         public void PrintRecipe()
         {
-            double totalCost = 0;
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
-                totalCost += step.GetProductionCost(step);
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
-            Console.WriteLine($"El costo total de la receta es:  + '{totalCost}'");
+            Console.WriteLine($"El costo total de la receta es: ${this.TotalCost}");
         }
     }
 }
